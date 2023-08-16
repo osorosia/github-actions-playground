74692c89f55604e1da3b956ccabfbff438440310
osorosia <64416398+osorosia@users.noreply.github.com>
2023-08-17 01:16:25 +0900

    push

---
+++ go/hoge/hoge.go
@@ -0,0 +1,5 @@
+package hoge
+
+func Foo() string {
+	return "foo"
+}
---
+++ go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"actions/hoge"
+	"fmt"
+)
+
+func add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println(add(1, 2))
+	fmt.Println(hoge.Foo())
+}
